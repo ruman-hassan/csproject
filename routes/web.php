6dd5549a2c1e91ff54adcab86b1187f0ffa028e2
Joyline Karanja <joyline.19.karanja@gmail.com>
2023-06-11 16:36:20 +0300

    Added files

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('register', 'Auth\RegisterController@register');
+Route::post('register', 'Auth\RegisterController@store')->name('register');
+
+Route::get('login', 'Auth\LoginController@login')->name('login');
+Route::post('login', 'Auth\LoginController@authenticate');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+
+Route::get('home', 'HomeController@home')->name('home');
+
+Route::get('forget-password', 'Auth\ForgotPasswordController@getEmail');
+Route::post('forget-password', 'Auth\ForgotPasswordController@postEmail');
+
+Route::get('reset-password/{token}', 'Auth\ResetPasswordController@getPassword');
+Route::post('reset-password', 'Auth\ResetPasswordController@updatePassword');
+
+Route::get('/', function () {
+    return view('welcome');
+});
